b7a0677efcd1352f53c1c68ffc1cb4bb40c2bf1c
Cristian Arango <cristian.arango1910@gmail.com>
2022-10-30 14:21:47 -0500

    Funcional Tab

--- code/Crimson/ProductRange/Controller/Customer/Index.php
+++ code/Crimson/ProductRange/Controller/Customer/Index.php
@@ -5,7 +5,7 @@ namespace Crimson\ProductRange\Controller\Customer;
 class Index extends \Magento\Framework\App\Action\Action 
 {
     public function execute() 
-    {
+    {   
         $this->_view->loadLayout();
         $this->_view->renderLayout();
     }
