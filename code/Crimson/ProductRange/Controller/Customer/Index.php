ed7435496a85b595825a086e090cf592f2c787a9
Cristian Arango <cristian.arango1910@gmail.com>
2022-10-30 05:53:51 -0500

    all folders into app

---
+++ code/Crimson/ProductRange/Controller/Customer/Index.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Crimson\ProductRange\Controller\Customer;
+
+class Index extends \Magento\Framework\App\Action\Action 
+{
+    public function execute() 
+    {
+        $this->_view->loadLayout();
+        $this->_view->renderLayout();
+    }
+}
