9ca0aa36dee112f37799febf207ec81438b2eeb1
Cristian Arango <cristian.arango1910@gmail.com>
2022-10-31 07:02:41 -0500

    filter products by status and visibility

--- code/Crimson/ProductRange/Controller/Customer/range.php
+++ code/Crimson/ProductRange/Controller/Customer/range.php
@@ -7,15 +7,21 @@ class Range extends \Magento\Framework\App\Action\Action
 {
     protected $_productCollectionFactory;
     protected $_storeManager;
+    protected $_productVisibility;
+    protected $_productStatus;
 
     public function __construct(
         \Magento\Framework\App\Action\Context $context,        
         \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
-        \Magento\Store\Model\StoreManagerInterface $storemanager
+        \Magento\Store\Model\StoreManagerInterface $storemanager,
+        \Magento\Catalog\Model\Product\Attribute\Source\Status $productStatus,
+        \Magento\Catalog\Model\Product\Visibility $productVisibility,
     )
     {    
         $this->_productCollectionFactory = $productCollectionFactory;
         $this->_storeManager =  $storemanager;
+        $this->_productStatus = $productStatus;
+        $this->_productVisibility = $productVisibility;
         parent::__construct($context);
     }
 
@@ -29,43 +35,49 @@ class Range extends \Magento\Framework\App\Action\Action
             $highRange = number_format((float)$params['highRange'], 2, '.', '');
             $sortByPrice = $params['sortByPrice']=='1 Ascending'?'ASC':'DESC';
 
-            $collection=$this->_productCollectionFactory->create();
-            $collection->addAttributeToSelect('*');
-            $collection->clear()
-            ->addPriceDataFieldFilter('%s >= %s', ['final_price', $lowRange])
-            ->addPriceDataFieldFilter('%s <= %s', ['final_price', $highRange])
-            ->addFinalPrice();
-            $sort='.final_price '.$sortByPrice;
-            $collection->getSelect()->order( $sort )->limit(10);
-
-            $productCollection=$collection;
-
-            foreach ($productCollection as $product) {
-                $p = $product->getData();   
-                $p['url'] = $product->getProductUrl();
-                $p['image'] = $store->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) . 'catalog/product' .$product->getImage();
-                $p['final_price']=number_format($p['final_price'], 2, ',', ' ');
-                $productsArray[] = $p;
-            }
-            $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
-
-            $response = $this->resultFactory->create(ResultFactory::TYPE_RAW);
-            $response->setHeader('Content-type', 'text/plain');
-            $response->setContents(
-                json_encode($productsArray)
-            );
+            $productCollection = $this->getProductCollection($lowRange, $highRange, $sortByPrice);
+            
+            $response=$this->getJsontoResponse($productCollection,$store);
+            
             return $response; 
         }
     }
 
 
-    public function getProductCollection($pageSize)
+    private function getProductCollection($lowRange, $highRange, $sortByPrice)
     {
-        $collection = $this->_productCollectionFactory->create();
+        $collection=$this->_productCollectionFactory->create();
         $collection->addAttributeToSelect('*');
-        $pageSize=$pageSize?$pageSize:5;
-        return $collection->clear()
-        ->setPageSize($pageSize)
-        ->load();
+        $collection->addAttributeToFilter('status', ['in' => $this->_productStatus->getVisibleStatusIds()]);
+        $collection->setVisibility($this->_productVisibility->getVisibleInSiteIds());
+        $collection->clear()
+        ->addPriceDataFieldFilter('%s >= %s', ['final_price', $lowRange])
+        ->addPriceDataFieldFilter('%s <= %s', ['final_price', $highRange])
+        ->addFinalPrice();
+        $sort='.final_price '.$sortByPrice;
+        $collection->getSelect()->order( $sort )->limit(10);
+        
+        return $collection;
     }
+
+    private function getJsontoResponse($productCollection, $store)
+    {
+        foreach ($productCollection as $product) {
+            $p = $product->getData();   
+            $p['url'] = $product->getProductUrl();
+            $p['image'] = $store->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) . 'catalog/product' .$product->getImage();
+            $p['final_price']=number_format($p['final_price'], 2, ',', ' ');
+            $productsArray[] = $p;
+        }
+        $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+
+        $response = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+        $response->setHeader('Content-type', 'text/plain');
+        $response->setContents(
+            json_encode($productsArray)
+        );
+
+        return $response;
+    }
+
 }
